d4ee5e2f548f473d6bf93dbb684193ad7468a737
Erik Klein <erik610.ek@gmail.com>
2022-05-05 17:28:52 +0200

    mute finalisiert.

--- gestures/impl/mute.py
+++ gestures/impl/mute.py
@@ -11,46 +11,50 @@ class MuteGesture(Gesture):
         self.addLastPosition(left, right)
 
         # check if recent positions contains enough values
-        if not self.lenLeftPosition() >= 10:
+        if not self.lenLeftPosition() >= 7:
+            return False
+        if not self.lenRightPosition() >= 7:
             return False
 
-        # check if movement goes from left to right
-        comparisonDistance = right.getComparisonDistance()*100/15
-        # check movement to right
-        valid = True
-        for i in range(1, 7):
-            if not self.getRightPosition(i).getMidOfHand().x >= self.getRightPosition(i-1).getMidOfHand().x+0.01*comparisonDistance:
-                valid = False
-        if valid and doValid:
-            self.onValid(True)
-            return True
+        if sum([1 for i in range(7) if self.getRightPosition(i) is None]) == 0:
+            # check if movement goes from left to right
+            comparisonDistance = self.getRightPosition(0).getComparisonDistance()*100/15
+            # check movement to right
+            valid = True
+            for i in range(1, 7):
+                if not self.getRightPosition(i).getMidOfHand().x >= self.getRightPosition(i-1).getMidOfHand().x+0.01*comparisonDistance:
+                    valid = False
+            if valid and doValid:
+                self.onValid(True)
+                return True
 
-        # check movement to left
-        valid = True
-        for i in range(1, 7):
-            if not self.getRightPosition(i).getMidOfHand().x <= self.getRightPosition(
-                    i - 1).getMidOfHand().x - 0.01 * comparisonDistance:
-                valid = False
-        if valid and doValid:
-            self.onValid(False)
-            return True
+            # check movement to left
+            valid = True
+            for i in range(1, 7):
+                if not self.getRightPosition(i).getMidOfHand().x <= self.getRightPosition(
+                        i - 1).getMidOfHand().x - 0.01 * comparisonDistance:
+                    valid = False
+            if valid and doValid:
+                self.onValid(False)
+                return True
 
-        comparisonDistance = left.getComparisonDistance()*100/15
-        valid = True
-        for i in range(1, 7):
-            if not self.getLeftPosition(i).getMidOfHand().x >= self.getLeftPosition(i-1).getMidOfHand().x+0.01*comparisonDistance:
-                valid = False
-        if valid and doValid:
-            self.onValid(True)
-            return True
-        valid = True
-        for i in range(1, 7):
-            if not self.getLeftPosition(i).getMidOfHand().x <= self.getLeftPosition(
-                    i - 1).getMidOfHand().x - 0.01 * comparisonDistance:
-                valid = False
-        if valid and doValid:
-            self.onValid(False)
-            return True
+        if sum([1 for i in range(7) if self.getLeftPosition(i) is None]) == 0:
+            comparisonDistance = self.getLeftPosition(0).getComparisonDistance()*100/15
+            valid = True
+            for i in range(1, 7):
+                if not self.getLeftPosition(i).getMidOfHand().x >= self.getLeftPosition(i-1).getMidOfHand().x+0.01*comparisonDistance:
+                    valid = False
+            if valid and doValid:
+                self.onValid(True)
+                return True
+            valid = True
+            for i in range(1, 7):
+                if not self.getLeftPosition(i).getMidOfHand().x <= self.getLeftPosition(
+                        i - 1).getMidOfHand().x - 0.01 * comparisonDistance:
+                    valid = False
+            if valid and doValid:
+                self.onValid(False)
+                return True
 
         return False
 
